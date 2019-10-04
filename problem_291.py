18ecc5ed2866b5e5fe0b01dc8d6ee8e58213b547
ccirelli2 <ccirelli2@gmail.com>
2019-10-03 20:57:10 -0400

    -q

--- problem_291.py
+++ problem_291.py
@@ -12,6 +12,24 @@ determine how many boats will be needed to save everyone.
 For example, given a population with weights [100, 200, 150, 80] and a boat limit of 200, the smallest number of boats required will be three. 
 '''
 
+# Import Modules
+import random
+
+
+# Create List of Randomly Generated Integers
+
+def gen_list_ran_ints(num_ints, lower, upper):
+	list_weights = []
+	for x in range(0, num_ints):
+		list_weights.append(random.randint(lower, upper))
+	return list_weights
+
+list_rand_nums = gen_list_ran_ints(100, 8, 200)
+
+
+
+
+
 
 
 
