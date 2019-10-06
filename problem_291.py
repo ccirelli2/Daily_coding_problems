8c810e3b4478685a3223995c35c083edec3312dc
Chris Cirelli <ccirelli2@gmail.com>
2019-10-06 07:50:25 -0400

    -q

--- problem_291.py
+++ problem_291.py
@@ -11,30 +11,6 @@ determine how many boats will be needed to save everyone.
 
 For example, given a population with weights [100, 200, 150, 80] and a boat limit of 200, the smallest number of boats required will be three. 
 '''
-'''
-    Objective:  Find the combination of weights that results in the least number
-                of life boats required. 
-                Max weight per boat is 200
-    Approach    Start with the first number in the list.  
-                Iterate the list starting with index value 1 and add it to the first number. 
-                If the sum is < 200, continue adding integers. 
-                If equal to 200, append the combination to a list holding combos. 
-                If greater than 200, append the first number to the list of combos. 
-
-    Step1:      Is the weight > 200 - min_weight?
-                If so, no other people can fit in the boat and the weight be < 200.
-                Therefore, one boat has been occupied an we should remove this person 
-                from the list. 
-                Append the weight to the list and move to the next one. 
-
-    Step2:      If weight < 200 - min_weight, then there may be an additional person that
-                can fit in the life raft.
-                We will want to look at every possible combination of the first weight in the
-                list with every other weight starting from position 1:, excluding first weight. 
-
-    Step2B:         
-
-'''
 
 # Import Modules
 import random
